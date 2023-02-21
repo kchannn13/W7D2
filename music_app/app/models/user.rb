ccc2bdfa5efeb7429bcf8da2d1c5f103d8ae122e
Kevin Chan <chankevin13@gmail.com>
2023-02-21 14:38:56 -0500

    CreatedUserModel

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,8 +1,60 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
-    validates :email, presence:true, uniqueness:true
+    validates :email, :session_token, presence:true, uniqueness:true
     validates :password_digest, presence:true
-    validate :session_token, presence:true, uniqueness:true
+
     validates :password, length: { minimum: 6 }, allow_nil:true
 
     before_validation :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def generate_unique_session_token
+        token = SecureRandom::urlsafe_base64
+        while User.exists?(session_token: token)
+            token = SecureRandom::urlsafe_base64
+        end
+        token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        bcrypt_obj = BCrypt::Password.new(self.password_digest)
+        bcrypt_obj.is_password?(password)
+    end
+
+
 end
